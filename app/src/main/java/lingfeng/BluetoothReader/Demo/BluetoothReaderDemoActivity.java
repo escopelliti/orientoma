3e893e2ff222bcad00004a07d2dc9b20efa4fa5b
gphilo <garollafilippo@outlook.com>
2015-11-02 22:15:02 +0100

    Minor fixes

--- app/src/main/java/lingfeng/BluetoothReader/Demo/BluetoothReaderDemoActivity.java
+++ app/src/main/java/lingfeng/BluetoothReader/Demo/BluetoothReaderDemoActivity.java
@@ -85,7 +85,7 @@ public class BluetoothReaderDemoActivity extends Activity {
         String node_id = null;
 
         node_id = activity.translator.getNodeId(uid);
-        node_id = uid; //TODO: Remove this when translator will be implemented
+        //node_id = uid; //TODO: Remove this when translator will be implemented
         Log.d(TAG, "Read tag with UID " + uid + " that was translated to Node ID " + node_id);
         if (node_id == null) {
             Log.e(TAG, "Translator could not find a match for tag with UID " + uid);
